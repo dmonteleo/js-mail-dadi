44b40990820431b448d677a83cd2c70efd2fed74
dmonteleo <dmonteleo@gmail.com>
2022-04-11 16:07:38 +0200

    mail exercise started 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,18 @@ const userMail = prompt('inserisci la tua mail');
 console.log(userMail);
 const lengthAuthorizedList = authorizedList.length;
 console.log(lengthAuthorizedList);
-const flag = false;
+let flag = false;
+let output = 'la mail inserita NON è nella lista delle mail autorizzate'
 for ( i=0; i<lengthAuthorizedList; i++ ){
   if ( userMail === authorizedList[i]){
     flag = true;
-    console.log('la mail inserita è nella lista delle mail autorizzate');
+    output = 'la mail inserita è nella lista delle mail autorizzate'
   }
-  else {
-    console.log('la mail inserita NON è nella lista delle mail autorizzate');
-  }
+}
+console.log(output);
+
+const prova = [];
+console.log(prova);
+for (i=0; i<10; i++){
+
 }
